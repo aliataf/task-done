2334b92b5578d81394b3042fdbc0ae338cf9d00e
Ali Ataf <aliattaf98@gmail.com>
2021-12-16 01:48:34 +0200

    :sparkles: feat: add closing all tabs and collapsing folders functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
-import * as vscode from 'vscode';
+import { commands, ExtensionContext } from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('task-done.taskDone', () => {
-		vscode.window.showInformationMessage('Hello World from Task Done!');
-	});
-	context.subscriptions.push(disposable);
+export function activate(context: ExtensionContext) {
+  let disposable = commands.registerCommand("task-done.taskDone", () => {
+    Promise.all([
+      commands.executeCommand("workbench.files.action.collapseExplorerFolders"),
+      commands.executeCommand("workbench.action.closeAllEditors"),
+    ]);
+  });
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
